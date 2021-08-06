113e269cd0f8593b55e3eb3d335b136a4b49ded1
Grumpy Programmer <very.grumpy.programmer@gmail.com>
2021-08-06 13:23:10 +0200

    chore: add spring boot maven project

---
+++ src/test/java/io/grumpyprogrammer/springmongodemo/SpringMongoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.grumpyprogrammer.springmongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
